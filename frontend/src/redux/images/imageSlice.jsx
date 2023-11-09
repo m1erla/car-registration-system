b04834730f73a0b7547df9c8ce078c727520b888
m1erla <116915007+m1erla@users.noreply.github.com>
2023-11-09 22:18:15 +0300

    revised, refactoring and deleted some imports and folders

--- frontend/src/redux/images/imageSlice.jsx
+++ frontend/src/redux/images/imageSlice.jsx
@@ -1,41 +1,40 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { addImage, axiosGetImage } from "../../api";
 
-export const imageSlice = createSlice({
+const initialState = {
+  imageId: [],
+  imageIdStatus: "idle",
+  imageIdError: null,
+};
+
+const imageSlice = createSlice({
   name: "image",
-  initialState: {
-    //getImageByCarId
-    imageId: [],
-    imageIdStatus: "idle",
-    imageIdError: null,
-  },
-
-  extraReducers: {
-    //get image by car id
-    [axiosGetImage.pending]: (state, action) => {
-      state.imageIdStatus = "loading";
-    },
-    [axiosGetImage.fulfilled]: (state, action) => {
-      state.imageId = action.payload;
-      state.imageIdStatus = "success";
-    },
-    [axiosGetImage.rejected]: (state, action) => {
-      state.imageIdStatus = "failed";
-      state.imageIdError = action.error.message;
-    },
-
-    //add one image
-    [addImage.pending]: (state, action) => {
-      state.imageIdStatus = "loading";
-    },
-    [addImage.fulfilled]: (state, action) => {
-      state.imageId.push(action.payload);
-      state.imageIdStatus = "success";
-    },
-    [addImage.rejected]: (state, action) => {
-      state.imageIdStatus = "failed";
-      state.imageIdError = action.error.message;
-    },
+  initialState,
+  reducers: {}, // Add any additional reducers if needed
+  extraReducers: (builder) => {
+    builder
+      .addCase(axiosGetImage.pending, (state) => {
+        state.imageIdStatus = "loading";
+      })
+      .addCase(axiosGetImage.fulfilled, (state, action) => {
+        state.imageId = action.payload;
+        state.imageIdStatus = "success";
+      })
+      .addCase(axiosGetImage.rejected, (state, action) => {
+        state.imageIdStatus = "failed";
+        state.imageIdError = action.error.message;
+      })
+      .addCase(addImage.pending, (state) => {
+        state.imageIdStatus = "loading";
+      })
+      .addCase(addImage.fulfilled, (state, action) => {
+        state.imageId.push(action.payload);
+        state.imageIdStatus = "success";
+      })
+      .addCase(addImage.rejected, (state, action) => {
+        state.imageIdStatus = "failed";
+        state.imageIdError = action.error.message;
+      });
   },
 });
 
--- frontend/src/redux/users/userSlice.jsx
+++ frontend/src/redux/users/userSlice.jsx
@@ -1,61 +1,60 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { changePassword, axiosRegister, loginAuth } from "../../api";
+import { axiosRegister, loginAuth, changePassword } from "../../api";
 
-export const userSlice = createSlice({
-  name: "user",
-  initialState: {
-    registerUser: null,
-    registerStatus: "idle",
-    registerError: null,
+const initialState = {
+  registerUser: null,
+  registerStatus: "idle",
+  registerError: null,
 
-    loginUser: null,
-    loginStatus: "idle",
-    loginError: null,
-  },
+  loginUser: null,
+  loginStatus: "idle",
+  loginError: null,
 
   changePasswordMessage: null,
   changePasswordStatus: "idle",
   changePasswordError: null,
+};
 
-  extraReducers: {
-    [axiosRegister.pending]: (state, action) => {
-      state.registerStatus = "loading";
-    },
-    [axiosRegister.fulfilled]: (state, action) => {
-      state.registerUser = action.payload;
-      state.registerStatus = "success";
-    },
-    [axiosRegister.rejected]: (state, action) => {
-      state.registerStatus = "failed";
-      state.registerError = action.error.message;
-    },
-
-    [loginAuth.pending]: (state, action) => {
-      state.loginStatus = "loading";
-    },
-    [loginAuth.fulfilled]: (state, action) => {
-      state.loginUser = action.payload;
-      state.loginStatus = "success";
-    },
-    [loginAuth.rejected]: (state, action) => {
-      state.loginStatus = "failed";
-      state.loginError = action.error.message;
-    },
-
-    [changePassword.pending]: (state, action) => {
-      state.changePasswordStatus = "loading";
-    },
-    [changePassword.fulfilled]: (state, action) => {
-      state.changePasswordMessage = action.payload;
-      state.changePasswordStatus = "success";
-    },
-    [changePassword.rejected]: (state, action) => {
-      state.changePasswordStatus = "failed";
-      state.changePasswordError = action.error.message;
-    },
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {}, // Add any additional reducers if needed
+  extraReducers: (builder) => {
+    builder
+      .addCase(axiosRegister.pending, (state) => {
+        state.registerStatus = "loading";
+      })
+      .addCase(axiosRegister.fulfilled, (state, action) => {
+        state.registerUser = action.payload;
+        state.registerStatus = "success";
+      })
+      .addCase(axiosRegister.rejected, (state, action) => {
+        state.registerStatus = "failed";
+        state.registerError = action.error.message;
+      })
+      .addCase(loginAuth.pending, (state) => {
+        state.loginStatus = "loading";
+      })
+      .addCase(loginAuth.fulfilled, (state, action) => {
+        state.loginUser = action.payload;
+        state.loginStatus = "success";
+      })
+      .addCase(loginAuth.rejected, (state, action) => {
+        state.loginStatus = "failed";
+        state.loginError = action.error.message;
+      })
+      .addCase(changePassword.pending, (state) => {
+        state.changePasswordStatus = "loading";
+      })
+      .addCase(changePassword.fulfilled, (state, action) => {
+        state.changePasswordMessage = action.payload;
+        state.changePasswordStatus = "success";
+      })
+      .addCase(changePassword.rejected, (state, action) => {
+        state.changePasswordStatus = "failed";
+        state.changePasswordError = action.error.message;
+      });
   },
 });
-
-export const {} = userSlice.actions;
 
 export default userSlice.reducer;
